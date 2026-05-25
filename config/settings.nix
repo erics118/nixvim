3694c86146619c75dfd934bcfd5e8b75259e1022
erics118 <52634785+erics118@users.noreply.github.com>
2026-05-25 17:03:54 -0400

    copy to system keyboard over ssh

--- config/settings.nix
+++ config/settings.nix
@@ -55,6 +55,18 @@
 
   # Custom border characters (rounded)
   extraConfigLua = ''
+    vim.g.clipboard = {
+      name = 'OSC 52',
+      copy = {
+        ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
+        ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
+      },
+      paste = {
+        ['+'] = require('vim.ui.clipboard.osc52').paste('+'),
+        ['*'] = require('vim.ui.clipboard.osc52').paste('*'),
+      },
+    }
+
     vim.g.bc = {
       style = "rounded",
       vert = "│",
